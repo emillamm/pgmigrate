63d9cbb0d3d1bb5dd55150f9c6d961457d472c60
Emil Lamm Nielsen <e@lamm.io>
2024-06-11 12:48:08 +0200

    refactor main

--- cmd/pgmigrate/main.go
+++ cmd/pgmigrate/main.go
@@ -1,44 +1,9 @@
 package main
 
 
-import (
-	"log"
-	"strconv"
-	_ "github.com/lib/pq"
-	"database/sql"
-	"fmt"
-	"github.com/emillamm/pgmigrate"
-	"github.com/emillamm/pgmigrate/env"
-)
+import "github.com/emillamm/pgmigrate"
 
 func main() {
-	user := env.GetenvOrFatal("POSTGRES_USER")
-	password := env.GetenvOrFatal("POSTGRES_PASSWORD")
-	host := env.GetenvWithDefault("POSTGRES_HOST", "localhost")
-	port, err := strconv.Atoi(env.GetenvWithDefault("POSTGRES_PORT", "5432"))
-	if err != nil {
-		log.Fatalf("invalid PORT %d", port)
-	}
-	migrationDir := env.GetenvWithDefault("POSTGRES_MIGRATION_DIR", "migrations")
-	retryAfterSeconds, err := strconv.Atoi(env.GetenvWithDefault("POSTGRES_MIGRATION_RETRY_INTERVAL", "120"))
-	if err != nil {
-		log.Fatalf("invalid POSTGRES_MIGRATION_RETRY_INTERVAL %d", port)
-	}
-
-	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%d sslmode=disable", user, password, host, port)
-	session, err := sql.Open("postgres", connStr)
-	if err == nil {
-		err = session.Ping()
-	}
-	if err != nil {
-		log.Fatalf("unable to connect to postgres database: %v", err)
-	}
-
-	provider := pgmigrate.FileMigrationProvider{Directory: migrationDir}
-	migrations := provider.GetMigrations()
-	err = pgmigrate.RunMigrations(session, migrations, retryAfterSeconds)
-	if err != nil {
-		log.Fatalf("unable to run migrations: %v", err)
-	}
+	pgmigrate.Run()
 }
 
